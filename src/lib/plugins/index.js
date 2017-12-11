0fb75fc530990653af57269405b1a4b39ba88f8d
Borna Almasi <borna@borna-almasi.com>
2017-12-11 17:52:35 -0500

    chore: forgot to export timeout plugin in Plugins index

--- src/lib/plugins/index.js
+++ src/lib/plugins/index.js
@@ -1,9 +1,11 @@
 const Logging = require('./logging.plugin');
 const Correlator = require('./correlator.plugin');
 const Package = require('./package.plugin');
+const Timeout = require('./timeout.plugin');
 
 module.exports = {
   Logging,
   Correlator,
-  Package
+  Package,
+  Timeout
 };
